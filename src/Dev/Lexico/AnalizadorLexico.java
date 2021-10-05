adfb6d13caa6e90f1138aeac530512564ef0249d
jere53 <53.salinas.jeremias@gmail.com>
2021-10-04 21:13:50 -0300

    Lo de juan

--- src/Dev/Lexico/AnalizadorLexico.java
+++ src/Dev/Lexico/AnalizadorLexico.java
@@ -229,6 +229,11 @@ public class AnalizadorLexico {
             leidos++;
 
             if (c.equals('\n')) {
+                // Para que no cuente los saltos de linea mas de una vez si devuelve un caracter a la entrada,
+                // despues de actualizar el numero de linea cambiamos el \n por un \t para que no lo vuelva a contar.
+                // el \t es equivalente al \n excepto en el caso de la Cadena de caracteres, pero como en el estado
+                // que corresponde a la Cadena se usa la AS3, que no devuelve el char a la entrada, no hay problema.
+                codigoFuente.set(indiceUltimoLeido + leidos - 1, '\t');
                 nroLinea++;
             }
 
