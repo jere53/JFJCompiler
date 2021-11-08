c24156a4e3425f7b79d577c7e7a712f9115c5a3e
jere53 <53.salinas.jeremias@gmail.com>
2021-11-07 23:12:39 -0300

    Arregla un par de errores de la seleccion, estan comentados en el codigo. Implementa el control para la iteracion.

--- src/TP3/Polaca.java
+++ src/TP3/Polaca.java
@@ -11,7 +11,8 @@ public class Polaca {
      * La lista contiene los punteros a TS y el indice esta dado por la propia ArrayList
      */
     private static final List<Object> representacionIntermedia = new ArrayList<>();
-    private static final Stack<Object> pila = new Stack<>(); //TODO , cambiar nombre a criterio de jere
+    private static final Stack<Integer> pila_seleccion = new Stack<>(); //TODO , cambiar nombre a criterio de jere
+    private static final Stack<Integer> pila_iteracion = new Stack<>(); //TODO , cambiar nombre a criterio de juan y felix
 
     public Polaca(){
 
@@ -22,29 +23,54 @@ public class Polaca {
     } // TODO, por que object y no Integer
 
     public static void insert_sentencia_control_cond(){
-        pila.add(representacionIntermedia.size()); // Agrego el index
+        pila_seleccion.add(representacionIntermedia.size()); // Agrego el index
 
         representacionIntermedia.add(new Object());
         representacionIntermedia.add(new String("BF"));
     }
 
     public static void insert_sentencia_control_then(){
-        int index = (int) pila.pop();
-        representacionIntermedia.add(index,  representacionIntermedia.size() + 1);
+        int index = (int) pila_seleccion.pop();
 
-        pila.add(representacionIntermedia.size()); // Agrego el index
+        //add hace un corrimiento, hay que cambiar el valor del objeto que se dejo vacio, por eso se usa set.
+        //representacionIntermedia.add(index,  representacionIntermedia.size() + 1);
+        //+2, 1 para el objeto vacio y otro para el BI. en size() va el objeto vacio, y en size()+1 el BI, si la condicion es
+        //falsa se empieza a leer desde size()+2
+        representacionIntermedia.set(index,  representacionIntermedia.size() + 2);
+
+        pila_seleccion.add(representacionIntermedia.size()); // Agrego el index
+
+
         representacionIntermedia.add(new Object());
         representacionIntermedia.add(new String("BI"));
     }
 
     public static void insert_sentencia_control_else(){
-        if(!pila.empty()){
-            int index = (int) pila.pop();
-            representacionIntermedia.add(index,  representacionIntermedia.size() + 1);
-        }
+        //La del ELSE es la que tiene que completar el destino de la BI, no agrega otra BF.
+        //Cond pregunta "hasta donde tengo que saltar si no se cumple la cond." Then le dice "tenes que saltar hasta aca si no se cumple"
+        //Then pregunta "a donde tengo que saltar si se cumple la cond, para ignorar el else?", Else le dice "si se cumplio, tenes que saltar
+        // hasta aca (desde donde Then dijo que salte)"
+        int index = (int) pila_seleccion.pop();
+        representacionIntermedia.set(index,  representacionIntermedia.size() + 1); //ya se leyo el codigo del ELSE, aca si es +1
+        //este valor hasta donde llega el else. Si se cumple la cond, lo que este entre el final del THEN y este valor se ignora.
+        //Si no hay rama ELSE, el codigo es el mismo, porque si la condicion se cumple se empieza a leer desde el final del then.
+        //no se necesita una regla para la rama sin else.
+
+        /*
         pila.add(representacionIntermedia.size());
         representacionIntermedia.add(new Object());
         representacionIntermedia.add(new String("BF"));
+        */
+
+    }
+
+    public static void insert_iteracion_start(){
+        pila_iteracion.push(representacionIntermedia.size()); //va a tener la primer sentencia del REPEAT
+    }
+
+    public static void insert_iteracion_end(){
+        Polaca.insert(pila_iteracion.pop()); //a donde tenemos que saltar si se cumple la cond
+        Polaca.insert(new String("BI"));
     }
 
     public static void removerUltimo(){
